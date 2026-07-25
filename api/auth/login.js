8a92d9b28160e118cd52af98d5590d7694cc7bba
hr450 <hr@rankviz.com>
2026-07-25 16:04:54 +0500

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -7,6 +7,11 @@ import { signToken } from "../../src/lib/authToken.js";
 import bcrypt from "bcryptjs";
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
   const { username, password } = req.body || {};
